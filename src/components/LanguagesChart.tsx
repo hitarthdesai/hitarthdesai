3539650fa1537ab9fcb6981785ea48dc4546fdda
Hitarth Desai <hitarth.desai@adevinta.com>
2023-07-30 15:03:57 -0400

    remove refs and add static sizes

--- src/components/LanguagesChart.tsx
+++ src/components/LanguagesChart.tsx
@@ -1,74 +1,63 @@
 import Pie, { ProvidedProps } from "@visx/shape/lib/shapes/Pie";
 import { GradientPinkBlue } from "@visx/gradient";
 import { LanguageData } from "@/util/types";
-import { useRef } from "react";
 
-export type PieProps = {
+export type LanguagesChartProps = {
+  width: number;
+  height: number;
   data: LanguageData[];
 };
 
-export const LanguagesChart = ({ data }: PieProps) => {
-  const languagesContainer = useRef<HTMLDivElement>(null);
-  const width = languagesContainer.current?.getBoundingClientRect().width ?? 0;
-  const height =
-    languagesContainer.current?.getBoundingClientRect().height ?? 0;
-
+export const LanguagesChart = ({
+  data,
+  width,
+  height,
+}: LanguagesChartProps) => {
   const radius = Math.min(width, height) / 2;
   const centerX = width / 2;
   const centerY = height / 2;
 
   return (
-    <div
-      ref={languagesContainer}
-      className="row-span-2 col-span-2 w-full h-full flex flex-wrap"
-    >
-      {languagesContainer.current && (
-        <svg width={width} height={height}>
-          <GradientPinkBlue id="visx-pie-gradient" />
-          <rect
-            rx={14}
-            width={width}
-            height={height}
-            fill="url('#visx-pie-gradient')"
-          />
-          <g transform={`translate(${centerX}, ${centerY})`}>
-            <Pie
-              data={data}
-              pieValue={(d) => d.usage}
-              outerRadius={radius - 16}
-            >
-              {({ arcs, path }: ProvidedProps<LanguageData>) => (
-                <>
-                  {arcs.map((arc, index) => {
-                    const [centroidX, centroidY] = path.centroid(arc);
-                    return (
-                      <g key={index}>
-                        <path
-                          d={path(arc) ?? ""}
-                          fill={`rgba(93, 30, 91, ${1 - index / arcs.length})`}
-                        />
-                        <g>
-                          <text
-                            fill="white"
-                            x={centroidX}
-                            y={centroidY}
-                            dy=".33em"
-                            fontSize={9}
-                            textAnchor="middle"
-                            pointerEvents="none"
-                          >
-                            {arc.data.name}
-                          </text>
-                        </g>
-                      </g>
-                    );
-                  })}
-                </>
-              )}
-            </Pie>
-          </g>
-        </svg>
-      )}
-    </div>
+    <svg width={width} height={height}>
+      <GradientPinkBlue id="visx-pie-gradient" />
+      <rect
+        rx={14}
+        width={width}
+        height={height}
+        fill="url('#visx-pie-gradient')"
+      />
+      <g transform={`translate(${centerX}, ${centerY})`}>
+        <Pie data={data} pieValue={(d) => d.usage} outerRadius={radius - 16}>
+          {({ arcs, path }: ProvidedProps<LanguageData>) => (
+            <>
+              {arcs.map((arc, index) => {
+                const [centroidX, centroidY] = path.centroid(arc);
+                return (
+                  <g key={index}>
+                    <path
+                      d={path(arc) ?? ""}
+                      fill={`rgba(93, 30, 91, ${1 - index / arcs.length})`}
+                    />
+                    <g>
+                      <text
+                        fill="white"
+                        x={centroidX}
+                        y={centroidY}
+                        dy=".33em"
+                        fontSize={9}
+                        textAnchor="middle"
+                        pointerEvents="none"
+                      >
+                        {arc.data.name}
+                      </text>
+                    </g>
+                  </g>
+                );
+              })}
+            </>
+          )}
+        </Pie>
+      </g>
+    </svg>
   );
 };
--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -7,43 +7,62 @@ type ShowcaseProps = {
   repo: Repository;
 };
 
-type ActiveShowcaseChart = "topics" | "languages";
-
 export const Showcase = ({
   repo: { name, description, topics, languages, ...rest },
 }: ShowcaseProps) => {
-  const [activeShowcaseChart, setActiveShowcaseChart] =
-    useState<ActiveShowcaseChart>("languages");
+  const [isTopicsCarouselActive, setIsTopicsCarouselActive] =
+    useState<boolean>(false);
   return (
-    <>
-      <main className="w-full h-full p-4 flex flex-col bg-red-900 text-2xl">
-        <div className="flex flex-row mb-2">
-          <strong className="pr-1">HittuDesai</strong>
-          <span>/</span>
-          <p className="pl-1">{name || "DEFAULT_REPO_NAME"}</p>
-        </div>
-        <div className="w-full h-full flex-grow grid grid-cols-2 gap-4 overflow-hidden">
-          <article className="grid place-items-center rounded-md text-lg p-4 bg-red-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-            reiciendis nam exercitationem quibusdam accusantium dicta unde nobis
-            dolore accusamus, magnam autem natus quis in quod ea aliquid
-            dignissimos? Ullam aliquam aspernatur quae possimus officiis
-            sapiente magni perspiciatis? Veritatis ut consequuntur magnam
-            labore. Natus, autem veritatis aut itaque quas iste molestias. Ipsum
-            mollitia a perferendis placeat expedita voluptatum quaerat dolore
-            doloremque quia sapiente animi assumenda delectus labore distinctio
-            corporis, aspernatur qui ad at sint nobis perspiciatis itaque!
-            Reiciendis voluptatum animi recusandae?
-          </article>
-          <div className="grid grid-rows-2 grid-cols-2 place-items-center rounded-md bg-red-500">
-            {activeShowcaseChart === "topics" ? (
-              <TopicsCarousel topics={topics} />
-            ) : (
-              <LanguagesChart data={languages} />
-            )}
+    <main className="w-full h-full p-4 flex flex-col text-2xl bg-gray-700">
+      <div className="flex flex-row mb-2 text-white">
+        <strong className="pr-1">HittuDesai</strong>
+        <span>/</span>
+        <p className="pl-1">{name || "DEFAULT_REPO_NAME"}</p>
+      </div>
+      <div className="w-full h-full flex-grow flex flex-row gap-4 overflow-hidden">
+        <article className="flex-grow rounded-md text-sm p-4 bg-red-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          reiciendis nam exercitationem quibusdam accusantium dicta unde nobis
+          dolore accusamus, magnam autem natus quis in quod ea aliquid
+          dignissimos? Ullam aliquam aspernatur quae possimus officiis sapiente
+          magni perspiciatis? Veritatis ut consequuntur magnam labore. Natus,
+          autem veritatis aut itaque quas iste molestias. Ipsum mollitia a
+          perferendis placeat expedita voluptatum quaerat dolore doloremque quia
+          sapiente animi assumenda delectus labore distinctio corporis,
+          aspernatur qui ad at sint nobis perspiciatis itaque! Reiciendis
+          voluptatum animi recusandae?
+        </article>
+        <div className="relative min-w-[400px] max-w-[400px] min-h-[400px] max-h-[400px]">
+          <div
+            className={`absolute bg-pink-300 rounded-md ${
+              isTopicsCarouselActive
+                ? "w-full h-full z-10"
+                : "w-1/2 h-1/2 bottom-0 right-0 z-20"
+            }`}
+            onClick={() => setIsTopicsCarouselActive((prev) => !prev)}
+          >
+            <TopicsCarousel
+              width={isTopicsCarouselActive ? 400 : 200}
+              height={isTopicsCarouselActive ? 400 : 200}
+              topics={topics}
+            />
+          </div>
+          <div
+            className={`absolute ${
+              !isTopicsCarouselActive
+                ? "w-full h-full z-10"
+                : "w-1/2 h-1/2 bottom-0 right-0 z-20"
+            }`}
+            onClick={() => setIsTopicsCarouselActive((prev) => !prev)}
+          >
+            <LanguagesChart
+              width={!isTopicsCarouselActive ? 400 : 200}
+              height={!isTopicsCarouselActive ? 400 : 200}
+              data={languages}
+            />
           </div>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 };
