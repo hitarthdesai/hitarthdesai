4db7a73d854e390b91940a4c196a861db19b31b2
Hitarth Desai <83507760+hitarthdesai@users.noreply.github.com>
2025-02-11 16:28:14 -0500

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { SiteHeader } from "@/components/SiteHeader";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={`${inter.className} flex h-dvh flex-col pb-4`}>
+        <Analytics />
         <SiteHeader />
         <div className="grow">{children}</div>
         <Toaster />
